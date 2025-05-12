8dd3da6016be1845bccb179c83f2b450c6016af2
ZermbaGerd <uuser7821@gmail.com>
2025-05-11 21:27:15 -0500

    prep fuel branch for pull request

--- scripts/installer.lua
+++ scripts/installer.lua
@@ -7,25 +7,25 @@
 function install()
 
     -- STARTUP (this runs when the computer turns on - gets data from a disk drive or just goes straight into main)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/startup.lua", "startup")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/startup.lua", "startup")
 
     -- MAIN (this is the main loop) (main)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/main.lua", "main")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/main.lua", "main")
     
     -- SMART ACTIONS (smac)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/smartActions.lua", "smartActions")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/smartActions.lua", "smartActions")
 
     -- CALIBRATION (calibration)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/calibration.lua", "calibration")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/calibration.lua", "calibration")
 
     -- GLOBALS (global)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/globals.lua", "globals")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/globals.lua", "globals")
 
     -- MINING STATE FUNCTIONS (smartMine)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/smartMine.lua", "smartMine")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/smartMine.lua", "smartMine")
 
     -- CRAFTING STATE FUNCTIONS (smartCraft)
-    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/fuel/scripts/smartCraft.lua", "smartCraft")
+    shell.run("wget","https://raw.githubusercontent.com/ZermbaGerd/scute-swarm/refs/heads/main/scripts/smartCraft.lua", "smartCraft")
 
 end
 
